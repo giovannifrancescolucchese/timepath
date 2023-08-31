0904c3e6b60925418d5806fc2e08e5ca60d22739
Giovanni F. Lucchese <info@lucchese.name>
2023-08-31 12:51:51 +0200

    last commit

--- src/test/java/it/euris/converter/DistanceConverterTest.java
+++ src/test/java/it/euris/converter/DistanceConverterTest.java
@@ -17,21 +17,24 @@ class DistanceConverterTest {
     }
 
 
-    @ParameterizedTest(name="Given {0} Km Then return {1} nautical miles ")
+    @ParameterizedTest(name="Given {0} Km Then return {1} nautical miles and {2} terrestral miles")
     @CsvSource({
-            "0.0,  0.0",
-            "1.852, 1.0",
-            "1852, 1000",
+            "0.0,  0.0, 0.0,",
+            "1.0, 0.5399568034557235, 0.621371192237334",
+            "1852, 1000, 1609,",
             Double.MAX_VALUE-1+ ", "+Double.POSITIVE_INFINITY,
             Double.MAX_VALUE+ ", "+Double.POSITIVE_INFINITY
     })
-    void convertKilometersToNauticalMiles(Double kilometers, Double expectedResult) throws DistanceConverterException {
+    void convertKilometersToNauticalMiles(Double kilometers, Double expectedResultMN, Double expectedResultM) throws DistanceConverterException {
         //arrange
         dc.setFromKilometers(kilometers);
         //act
         Double result=dc.getNauticalMiles();
+        Double result2=dc.getMiles();
         //assert
-        assertEquals(expectedResult, result, ()->String.format("%1$,.2f km  should equals to %2$,.2f nautical miles", kilometers,expectedResult));
+        assertEquals(expectedResultMN, result, ()->String.format("%1$,.2f km  should equals to %2$,.2f nautical miles", kilometers,expectedResultMN));
+        assertEquals(expectedResultM, result2, ()->String.format("%1$,.2f km  should equals to %2$,.2f nautical miles", kilometers,expectedResultM));
+
     }
 
 
