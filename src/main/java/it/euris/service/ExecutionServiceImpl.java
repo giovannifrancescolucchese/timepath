694c6e6b7d3f53afe324a42108426a0f4184eac6
Giovanni F. Lucchese <info@lucchese.name>
2023-09-04 23:31:45 +0200

    spy test doubles

--- src/main/java/it/euris/service/ExecutionServiceImpl.java
+++ src/main/java/it/euris/service/ExecutionServiceImpl.java
@@ -1,17 +1,22 @@
 package it.euris.service;
 
-import java.util.Random;
+import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.Random;
+@Data
 public class ExecutionServiceImpl implements ExecutionService {
 
     private Integer executions;
+    private Integer seed;
+    private LocalDate date;
 
     public ExecutionServiceImpl() {
         executions=0;
     }
     @Override
     public Integer getPercentage() {
-        Random random=new Random();
+        Random random=new Random(seed);
         return random.nextInt()%100;
     }
 
--- src/test/java/it/euris/service/TimePathCalculatorServiceTest.java
+++ src/test/java/it/euris/service/TimePathCalculatorServiceTest.java
@@ -4,24 +4,37 @@ import it.euris.exception.DistanceConverterException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class TimePathCalculatorServiceTest {
+
+
+    @Mock
+    ExecutionServiceImpl executionServiceMock;
+
+    @Spy
+    ExecutionServiceImpl executionServiceSpy;
 
     @Test
     @DisplayName("convert String 0h 0m 0s the return 0h 0m 0s and set the correct attributes")
-    @ExtendWith(MockitoExtension.class)
     public void givenExecuteWhen500KMAnd50KMHThenReturn10h0m0s() throws DistanceConverterException {
         //arrange
         String distancetring="500 KM";
         String speedString="50 KMH";
         String expectedResult="10h 0m 0s";
+        executionServiceMock.setExecutions(100);
+        executionServiceSpy.setExecutions(100);
+        assertEquals(executionServiceMock.getExecutions(),0);
+        assertEquals(executionServiceSpy.getExecutions(),100);
+
         TimePathCalculatorService timePathCalculatorService=new TimePathCalculatorService(distancetring, speedString);
-        final ExecutionServiceImpl executionServiceMock=Mockito.mock(ExecutionServiceImpl.class);
         Mockito.when(executionServiceMock.getPercentage()).thenReturn(79);
         //act
         String result=timePathCalculatorService.execute(executionServiceMock);
@@ -30,6 +43,7 @@ class TimePathCalculatorServiceTest {
         verify(executionServiceMock).increment();
         assertEquals(expectedResult,result, "tempo di percorrenza calcolato correttamente");
     }
+
 
     //TODO: continuare con i test
 
